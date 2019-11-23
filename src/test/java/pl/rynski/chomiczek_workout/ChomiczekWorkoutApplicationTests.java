36cd99d72a03360bba64890b2b8d151e94ecd234
Michal Rynski <michalrynski96@gmail.com>
2019-11-23 20:02:18 +0100

    Initial commit

---
+++ src/test/java/pl/rynski/chomiczek_workout/ChomiczekWorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rynski.chomiczek_workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChomiczekWorkoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
