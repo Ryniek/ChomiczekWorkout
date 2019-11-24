d7f50d35bc8808a1e497bf296c9812c05d68776d
Michal Rynski <michalrynski96@gmail.com>
2019-11-24 13:06:50 +0100

    CHOM-40 Stworzenie użytkownika - Prosta rejestracja zrobiona.

--- src/main/java/pl/rynski/chomiczek_workout/security/SecurityConfig.java
+++ src/main/java/pl/rynski/chomiczek_workout/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.rynski.chomiczek_workout.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,5 +13,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
     }
 }
