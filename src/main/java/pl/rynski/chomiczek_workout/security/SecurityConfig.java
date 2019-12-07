85731900e6cf1cc0697768937fc7d7ddb8063ee0
Michal Rynski <michalrynski96@gmail.com>
2019-12-07 18:00:31 +0100

    Naprawa

--- src/main/java/pl/rynski/chomiczek_workout/security/SecurityConfig.java
+++ src/main/java/pl/rynski/chomiczek_workout/security/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/styles/**", "/images/**").permitAll()
                 .antMatchers("/").permitAll()
                 .antMatchers("/register").permitAll()
                 .anyRequest().authenticated()
