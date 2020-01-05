e74c9b74bc9ccf6f34bf01138c06c8538804a759
Michal Rynski <michalrynski96@gmail.com>
2020-01-05 20:16:07 +0100

    CHOM-65 Walidacja formularza profilu - backend

--- src/main/java/pl/rynski/chomiczek_workout/profile/controller/ProfileController.java
+++ src/main/java/pl/rynski/chomiczek_workout/profile/controller/ProfileController.java
@@ -3,11 +3,18 @@ package pl.rynski.chomiczek_workout.profile.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.rynski.chomiczek_workout.profile.modelDto.ErrorMessage;
 import pl.rynski.chomiczek_workout.profile.modelDto.ProfileDto;
 import pl.rynski.chomiczek_workout.profile.service.ProfileService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class ProfileController {
@@ -26,14 +33,23 @@ public class ProfileController {
     }
 
     @GetMapping("/updateProfile")
-    public String getUpdateSite(Model model) {
+    public String getUpdateSite(@ModelAttribute("errorMessage") ErrorMessage errorMessage, Model model) {
         model.addAttribute("profile", new ProfileDto());
+        model.addAttribute("errorMessage", errorMessage);
         return "profileUpdate";
     }
 
     @PostMapping("/updateProfile")
-    public String updateProfile(@ModelAttribute ProfileDto profileDto) {
-        profileService.updateProfile(profileDto);
-        return "redirect:/profile";
+    public String updateProfile(@Valid @ModelAttribute ProfileDto profileDto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        if (!bindingResult.hasErrors()) {
+            profileService.updateProfile(profileDto);
+            return "redirect:/profile";
+        } else {
+            List<ObjectError> errors = bindingResult.getAllErrors();
+            ObjectError error = errors.get(0);
+            ErrorMessage errorMessage = new ErrorMessage(error.getDefaultMessage());
+            redirectAttributes.addFlashAttribute("errorMessage", errorMessage);
+            return "redirect:/updateProfile";
+        }
     }
 }
